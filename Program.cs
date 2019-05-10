c4c2c9f0760be973f702d6cfa12a20ace28b87e1
sealanguage <elaine.froehlich@gmail.com>
2019-05-09 20:31:07 -0400

    Initial check-in of module NextCSharpHello

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NextCSharpHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("trying to upload to a new repository!");
+        }
+    }
+}
